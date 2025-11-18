ee2c3c65302cfd01d2a4f15fcb3027f0f501815b
cristiannzx <cruzmonturo@gmail.com>
2025-11-17 21:36:28 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juegos recreativos</title>
+    <link rel="stylesheet" href="css/juego.css">
+</head>
+<body>
+    <header>
+        <div class="title">🎮 JUEGOS CASUALES Y RECREATIVOS</div>
+
+    </header>
+
+    <h2>NUESTROS JUEGOS</h2>
+
+    
+
+    <section class="games">
+        <div class="card">
+            <img src="img/michi.png" alt="michi">
+            <div class="info">
+                <div class="game-title">TRES EN RAYA</div>
+                <br>
+                <a href="game/michi.php" class="btn">Jugar</a>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="img/sopa de letras.png" alt="sopa de letras">
+            <div class="info">
+                <div class="game-title">SOPA DE LETRAS</div>
+                <br>
+                <a href="sopa/menu.php" class="btn">Jugar</a>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="img/hanged.png" alt="hanged">
+            <div class="info">
+                <div class="game-title">AHORCADO</div>
+                <br>
+                <a href="hanged/menu.php" class="btn">Jugar</a>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="img/sudoku.png" alt="sudoku">
+            <div class="info">
+                <div class="game-title">SUDOKU</div>
+                <br>
+                <a href="game/sudoku.php" class="btn">Jugar</a>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
